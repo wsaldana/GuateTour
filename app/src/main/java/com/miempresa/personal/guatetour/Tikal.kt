350703c9c31b67fa4a715ca4b9785214f559088c
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-02-02 14:28:53 -0600

    Creación de clase Tikal

---
+++ app/src/main/java/com/miempresa/personal/guatetour/Tikal.kt
@@ -0,0 +1,12 @@
+package com.miempresa.personal.guatetour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tikal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tikal)
+    }
+}
