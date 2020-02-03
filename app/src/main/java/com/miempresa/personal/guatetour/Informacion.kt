198886834f55edccd32653e479476ec40b3b9aa3
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-02-02 23:49:52 -0600

    Binding

--- app/src/main/java/com/miempresa/personal/guatetour/Informacion.kt
+++ app/src/main/java/com/miempresa/personal/guatetour/Informacion.kt
@@ -1,42 +1,58 @@
 package com.miempresa.personal.guatetour
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import com.miempresa.personal.guatetour.databinding.ActivityInformacionBinding
 
 class Informacion : AppCompatActivity() {
 
+    lateinit var entrada: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_informacion)
+        entrada = findViewById(R.id.editText2)
         val binding: ActivityInformacionBinding = DataBindingUtil.setContentView(this, R.layout.activity_informacion)
 
-        //val key:String = intent.getStringExtra("nombre")
         val bundle: Bundle ?=intent.extras
         val key: String? = bundle?.getString("nombre")
 
         var titulo: String = when(key){
-            "tikal"->R.string.titulo_tikal
-            "antigua"->R.string.titulo_antigua
-            "xela"->R.string.titulo_xela
+            "tikal"->getString(R.string.titulo_tikal)
+            "antigua"->getString(R.string.titulo_antigua)
+            "xela"->getString(R.string.titulo_xela)
+            else -> getString(R.string.titulo_tikal)
         }
 
         var frase: String = when(key){
-            "tikal"->R.string.frase_tikal
-            "antigua"->R.string.frase_antigua
-            "xela"->R.string.frase_xela
+            "tikal"->getString(R.string.frase_tikal)
+            "antigua"->getString(R.string.frase_antigua)
+            "xela"->getString(R.string.frase_xela)
+            else->getString(R.string.frase_tikal)
         }
 
         var descr: String = when(key){
-            "tikal"->R.string.tikal
-            "antigua"->R.string.antigua
-            "xela"->R.string.xela
+            "tikal"->getString(R.string.tikal)
+            "antigua"->getString(R.string.antigua)
+            "xela"->getString(R.string.xela)
+            else->getString(R.string.tikal)
         }
 
         var modelo = Modelo()
         modelo.title = titulo
         modelo.phrase = frase
         modelo.description = descr
+
+        binding.modelo=modelo
+    }
+
+    fun comentar(view: View) {
+        val ventana = Intent(this, MainActivity::class.java)
+        ventana.putExtra("msm",entrada.text)
+        startActivity(ventana)
     }
 }
--- app/src/main/java/com/miempresa/personal/guatetour/MainActivity.kt
+++ app/src/main/java/com/miempresa/personal/guatetour/MainActivity.kt
@@ -6,32 +6,67 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var nombre: TextView
+    lateinit var label: TextView
+    lateinit var entrada: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nombre = findViewById(R.id.textView3)
+        label = findViewById(R.id.textView2)
+        entrada = findViewById(R.id.editText)
+
+        val bundle: Bundle ?=intent.extras
+        val key: String? = bundle?.getString("msm")
+
+        if(key.equals("")){
+            Toast.makeText(this, "Sin comentarios...", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, key, Toast.LENGTH_SHORT).show()
+        }
     }
 
-    fun ventanaTikal(){
+    fun ventanaTikal(view: View){
         val ventana = Intent(this, Informacion::class.java)
         ventana.putExtra("nombre","tikal")
         startActivity(ventana)
     }
 
-    fun ventanaAntigua(){
+    fun ventanaAntigua(view: View){
         val ventana = Intent(this, Informacion::class.java)
         ventana.putExtra("nombre","antigua")
         startActivity(ventana)
     }
 
-    fun ventanaXela(){
+    fun ventanaXela(view: View){
         val ventana = Intent(this, Informacion::class.java)
         ventana.putExtra("nombre","xela")
         startActivity(ventana)
+    }
+
+    fun editarNombre(view: View){
+        if (nombre.isVisible) {
+            entrada.visibility = View.VISIBLE
+            label.visibility = View.VISIBLE
+            nombre.visibility = View.INVISIBLE
+        } else {
+            entrada.visibility = View.INVISIBLE
+            label.visibility = View.INVISIBLE
+            nombre.text = entrada.text
+            nombre.visibility = View.VISIBLE
+        }
     }
 
 }
